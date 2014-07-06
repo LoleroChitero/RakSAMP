4f672b61d925a8478143828bb0aa721394ad92de
P3ti <p3ti@hotmail.hu>
2014-07-06 09:30:56 +0200

    getRuleValue, modifyRuleValue added

--- server/src/query.h
+++ server/src/query.h
@@ -4,4 +4,7 @@
 
 extern char serverName[256];
 
+char* getRuleValue(char* szSRule);
+void modifyRuleValue(char* szSRule, char* szMValue);
+
 void handleQueries(SOCKET sListen, int iAddrSize, struct sockaddr_in client, char *pBuffer);
