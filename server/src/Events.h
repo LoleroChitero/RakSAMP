53fd0d1828c4a38c01f53044857e316c3763b66d
P3ti <p3ti@hotmail.hu>
2014-07-06 09:15:31 +0200

    ScriptEvent_OnPlayerWeaponShot
    (ScriptEvent_OnPlayerDeath was edited too.)

--- server/src/Events.h
+++ server/src/Events.h
@@ -10,8 +10,9 @@ void ScriptEvent_OnNewConnection(lua_State *L, int iPlayerID, char *pszIP, int i
 void ScriptEvent_OnPlayerJoin(lua_State *L, int iPlayerID, char *pszPlayerName, char *pszIP, int iSourcePort);
 void ScriptEvent_OnPlayerDisconnect(lua_State *L, int iPlayerID, char *pszPlayerName, char *pszReason);
 void ScriptEvent_OnPlayerSpawn(lua_State *L, int iPlayerID);
-void ScriptEvent_OnPlayerDeath(lua_State *L, int iPlayerID, int iKillerID, int iWeaponID);
+void ScriptEvent_OnPlayerDeath(lua_State *L, int iPlayerID, int iKillerID, int iReasonID);
 void ScriptEvent_OnPlayerWantsEnterVehicle(lua_State *L, int iPlayerID, int iVehicleID, int iAsPassenger);
 void ScriptEvent_OnPlayerLeaveVehicle(lua_State *L, int iPlayerID, int iVehicleID);
 void ScriptEvent_OnPlayerMessage(lua_State *L, int iPlayerID, char *pszMessage);
 void ScriptEvent_OnPlayerCommand(lua_State *L, int iPlayerID, char *pszCommand);
+void ScriptEvent_OnPlayerWeaponShot(lua_State *L, int iPlayerID, int iWeaponID, int bHitType, int iHitID, float fX, float fY, float fZ);
